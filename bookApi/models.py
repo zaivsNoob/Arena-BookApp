673f3e7a18dd0f974a997de9df7f9a6143fa5790
zaivs <zaivsguitar@gmail.com>
2023-06-24 02:26:59 +0600

    Add models
    added Book and Cartitem models to the api app for keeping data in database

--- bookApi/models.py
+++ bookApi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class CartItem(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    
+
