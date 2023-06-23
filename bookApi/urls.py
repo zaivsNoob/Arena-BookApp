a7ba202d7a94a10c006cef63819824504763c72f
zaivs <zaivsguitar@gmail.com>
2023-06-24 04:03:28 +0600

    Add cart functionalities
    in the cart/ endpoint a book can be added with its quantity

--- bookApi/urls.py
+++ bookApi/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import book_list 
+from .views import book_list ,cart_all
 
 urlpatterns = [
     path('books/', book_list, name='book-list'),
+       path('cart/',cart_all , name='add-to-cart'),
  
 ]
--- bookApi/views.py
+++ bookApi/views.py
@@ -18,11 +18,16 @@ def book_list(request):
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)        
 
+@api_view(['GET','POST'])
+def cart_all(request):
+    if request.method=="GET":
+        items = CartItem.objects.all()
+        serializer = CartItemSerializer(items, many=True)
+        return Response(serializer.data)
 
-# @api_view(['POST'])
-# def add_book(request):
-#     serializer = BookSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data, status=201)
-#     return Response(serializer.errors, status=400)
+    elif request.method=="POST":
+        serializer = CartItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
